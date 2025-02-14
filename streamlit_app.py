39b5bd9638bf779b4b0527161ccf80f8526dcb2a
bethanavel <bethanavel@gmail.com>
2025-02-14 10:10:50 +0000

    flask api

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def chat_with_backend(question):
         }
         
         response = requests.post(
-            'http://127.0.0.1:5000/chat',
+            'https://9cee-103-176-188-225.ngrok-free.app/chat',
             json=payload,
             headers=headers,
             timeout=30
